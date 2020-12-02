a126d697466d4e4d387b90cf016790bccea2ca06
Merrick Clark <raccer@gmail.com>
2020-12-02 11:43:44 -0800

    Walkthrough demo changes

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,16 @@
 import React from "react"
+import { Link } from "gatsby"
+import Header from "../components/header"
 
 export default function Home() {
-  return <div>Hello world!</div>
+  return (
+    <div>
+      <Header headerText="About Gatsby!" />
+      <Header headerText="It's pretty cool" />
+      <Link to="/contact/">Contact</Link>
+      <div>Hello world!</div>
+      <div><h1>:-)</h1></div>
+      <img src="https://source.unsplash.com/random/400x200" alt="" />
+    </div>
+  );
 }
